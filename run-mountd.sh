1449e48f88a606e7032973e3341c1c92b5f8ea4a
namazu510 <namazu510@hotmail.co.jp>
2017-04-29 12:01:56 +0900

    add multi-path export

--- run-mountd.sh
+++ run-mountd.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "$1 *(rw,sync,no_subtree_check,no_root_squash,fsid=0)" >> /etc/exports
+set -e
+
+for mnt in "${mounts[@]}"; do
+    src=$(echo $mnt | awk -F':' '{ print $1 }')
+    mkdir -p $src
+    echo "$src *(rw,sync,no_subtree_check,no_root_squash,fsid=0)" >> /etc/exports
+done
+
 exportfs -ar
 exec /usr/lib/systemd/systemd --system --unit=nfs-server.service
